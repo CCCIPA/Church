94cc9b4174da7ed8e1623c54c4d513a2631b390b
StephenKarim <stephen.karim07@gmail.com>
2024-01-21 10:28:35 -0400

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,53 @@
-import logo from "@/assets/logo.png";
-import { Button } from "@/components/ui/button";
-import { auth } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  const { userId } = auth();
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-  if (userId) redirect("/notes");
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
 
-  return (
-    <main className="flex h-screen flex-col items-center justify-center gap-5">
-      <div className="flex items-center gap-4">
-        <Image src={logo} alt="FlowBrain logo" width={100} height={100} />
-        <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          FlowBrain
-        </span>
-      </div>
-      <p className="max-w-prose text-center">
-        An intelligent note-taking app with AI integration, built with OpenAI,
-        Pinecone, Next.js, Shadcn UI, Clerk, and more.
-      </p>
-      <Button size="lg" asChild>
-        <Link href="/notes">Open</Link>
-      </Button>
-    </main>
-  );
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
+
+// import logo from "@/assets/logo.png";
+// import { Button } from "@/components/ui/button";
+// import { auth } from "@clerk/nextjs";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { redirect } from "next/navigation";
+
+// export default function Home() {
+//   // const { userId } = auth();
+
+//   // if (userId) redirect("/notes");
+
+//   return (
+//     <main className="flex h-screen flex-col items-center justify-center gap-5">
+//       <div className="flex items-center gap-4">
+//         <Image src={logo} alt="FlowBrain logo" width={100} height={100} />
+//         <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+//           FlowBrain
+//         </span>
+//       </div>
+//       <p className="max-w-prose text-center">
+//         An intelligent note-taking app with AI integration, built with OpenAI,
+//         Pinecone, Next.js, Shadcn UI, Clerk, and more.
+//       </p>
+//       <Button size="lg" asChild>
+//         <Link href="/notes">Open</Link>
+//       </Button>
+//     </main>
+//   );
+// }
